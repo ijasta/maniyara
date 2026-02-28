bf6c6ffbef7deedd2f3b1ce98523ea20ecc03c1f
ijasta <IJASTA3@GMAIL.COM>
2026-02-28 19:38:17 +0530

    Add CommonFund route and import to App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from './pages/Dashboard'
 import MyTask from './pages/MyTask'
 import Members from './pages/Members'
 import Expenses from './pages/Expenses'
+import CommonFund from './pages/CommonFund'
 import AdminPanel from './pages/AdminPanel'
 import LoadingScreen from './components/LoadingScreen'
 
@@ -29,10 +30,11 @@ export default function App() {
       <Route path="/auth" element={user && profile?.status === 'approved' ? <Navigate to="/" replace /> : <AuthPage />} />
       <Route path="/pending" element={<PendingPage />} />
       <Route path="/" element={<Guard><Layout /></Guard>}>
-        <Route index        element={<Dashboard />} />
+        <Route index           element={<Dashboard />} />
         <Route path="mytask"   element={<MyTask />} />
         <Route path="members"  element={<Members />} />
         <Route path="expenses" element={<Expenses />} />
+        <Route path="fund"     element={<CommonFund />} />
         <Route path="admin"    element={<Guard adminOnly><AdminPanel /></Guard>} />
       </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
