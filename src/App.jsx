bf49afd724759e33aef83029c3c2d0f502630172
ijasta <IJASTA3@GMAIL.COM>
2026-03-05 10:49:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import AdminPanel from './pages/AdminPanel'
 import TaskAssigner from './pages/TaskAssigner'
 import MaintenancePage from './pages/MaintenancePage'
 import LoadingScreen from './components/LoadingScreen'
+import CookingParty from './pages/CookingParty'
 
 function Guard({ children, adminOnly }) {
   const { user, profile, loading } = useAuth()
